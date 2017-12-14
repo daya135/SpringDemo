e8f1bb179a39ef0cf4ad7de8dd69a087d89f2497
daya135 <Jzzperson@163.com>
2017-12-14 13:29:02 +0800

    add sql

--- springDemo_sql.sql
+++ springDemo_sql.sql
@@ -1,40 +1,39 @@
 drop table song_info;
 create table song_info (
 	songId integer,
-	title varchar(100),
-	artist varchar(100),
-	album varchar(100),
-	band varchar(100),
-	rate varchar(20),
-	len varchar(20),
-	publishYear varchar(20),
-	downSite varchar(10),
+	title varchar(255),
+	artist varchar(255),
+	album varchar(255),
+	band varchar(255),
+	rate varchar(255),
+	len varchar(255),
+	publishYear varchar(255),
+	downSite varchar(255),
 	onsale char(1),
-	langType varchar(10),
-	fileType varchar(10),
+	langType varchar(255),
+	fileType varchar(255),
 	isDownload char(1),
-	localPath varchar(200),
+	localPath varchar(255),
 	createTime timestamp,
 	updateTime timestamp
 );
 create table song_lable (
 	songId integer,
-	labelName varchar(50),
+	labelName varchar(255),
 	labelValue char(1),
 	createTime timestamp
 );
-
 ALTER TABLE `song_info` ADD PRIMARY KEY(`songId`);
 ALTER TABLE `song_info` CHANGE `songId` `songId` INT(11) NOT NULL AUTO_INCREMENT;
+truncate table song_info;
 
-select * from song_info where title like '%東方妖々夢%';
-select * from song_info where onsale is null;
-select * from song_info where isDownload = 1;
+select songid,title,artist,album from song_info order by songid desc;
 select count(*) from song_info;
-select * from song_info where title in (
+select count(*) from song_info where album is null;
+select * from song_info where album is null;
+select * from song_info where isDownload = 1;
+select songid,title,artist,album from song_info where title in (
 	select title from song_info group by title having count(*) > 1);
-delete from 	song_info;
-	
 	
 create table user_test (
 	loginid varchar(50),
@@ -50,13 +49,15 @@ create table user_test (
 );
 insert into user_test(loginid, `password`) values ("jzz", "123");
 
-
 create table file_upload (
-	fileName varchar(100),
-	fileType varchar(100),
-	filePath varchar(100),
+	fileName varchar(255),
+	fileType varchar(255),
+	filePath varchar(255),
 	downloadTimes int,
 	create_time timestamp,
 	update_time timestamp
 );
+
+select * from user_test;
+select * from address_test;
 	
